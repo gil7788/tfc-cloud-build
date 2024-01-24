3cfc44c5b4a33975701f54bd28de82bc271be6ee
gil7788 <gil7788@gmail.com>
2024-01-24 13:45:25 +0200

    Create Config File for Environments

---
+++ environments/config.sh
@@ -0,0 +1,15 @@
+# Description: This file contains the configuration variables for the Terraform Cloud (TFC) and Google Cloud Platform (GCP) environments.
+
+# Terraform Cloud (TFC)
+$TERRAFORM_ORGANIZATION_NAME = "My-TFC-Tutorial"
+$TERRAFORM_ORGANIZATION_ID = "org-Qzi2vYQye5iErTKo"
+$TFC_PROJECT_NAME = "tfc-cloud-build"
+$TFC_WORKSPACE_ID = "ws-7iS8HBL8YEAGdpSy"
+
+# Google Cloud Platform (GCP)
+$GCP_PROJECT_ID = "tfc-cloud-build"
+$GCP_ZONE=""
+$GCP_REGION=""
+
+#GitHub
+$GITHUB_REPOSITORY_URL = ""
