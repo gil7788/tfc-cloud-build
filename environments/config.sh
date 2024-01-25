7518c2b808e20334024e9ed60c00757b3cdb46d5
gil7788 <gil7788@gmail.com>
2024-01-25 10:57:23 +0200

    Use Env Vars in cloudbuild

--- environments/config.sh
+++ environments/config.sh
@@ -1,15 +1,22 @@
 # Description: This file contains the configuration variables for the Terraform Cloud (TFC) and Google Cloud Platform (GCP) environments.
 
 # Terraform Cloud (TFC)
-$TERRAFORM_ORGANIZATION_NAME = "My-TFC-Tutorial"
-$TERRAFORM_ORGANIZATION_ID = "org-Qzi2vYQye5iErTKo"
-$TFC_PROJECT_NAME = "tfc-cloud-build"
-$TFC_WORKSPACE_ID = "ws-7iS8HBL8YEAGdpSy"
+TERRAFORM_ORGANIZATION_NAME="My-TFC-Tutorial"
+TERRAFORM_ORGANIZATION_ID="org-Qzi2vYQye5iErTKo"
+TFC_PROJECT_NAME="tfc-cloud-build"
+TFC_WORKSPACE_ID="ws-7iS8HBL8YEAGdpSy"
 
 # Google Cloud Platform (GCP)
-$GCP_PROJECT_ID = "tfc-cloud-build"
-$GCP_ZONE=""
-$GCP_REGION=""
+PROJECT_ID="tfc-cloud-build"
+GCP_REGION="us-west2"
+GCP_ZONE="us-west2-a"
 
-#GitHub
-$GITHUB_REPOSITORY_URL = ""
+## Resources
+GCP_CLOUD_RUN_SERVICE_NAME="my-service"
+GCP_CONTAINER_REGISTRY_REPOSITORY_NAME="quickstart-docker-repo"
+
+# Docker
+DOCKER_IMAGE_NAME="quickstart-image"
+
+# GitHub
+GITHUB_REPOSITORY_URL="https://github.com/gil7788/tfc-cloud-build.git"
--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 echo "Hello, world! The time is $(date)."
+
+
+# Test environment variables
+source environmets/config.sh
+
+# Iterate through and print all environment variables
+for var in $(compgen -A variable); do
+  echo "$var=${!var}"
+done
