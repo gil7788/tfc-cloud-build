505771cd56a30f9fb61551545a8b0f6199b6f6f8
gil7788 <gil7788@gmail.com>
2024-01-25 14:42:02 +0200

    Enhance run.sh
    - Install and serve http files on port 8080 with darkhttpd
    - Copy config.sh to container with docker file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM alpine
 COPY run.sh /
 RUN chmod +x /run.sh
+
+COPY environments/config.sh /environments
+RUN chmod +x /environments/config.sh
 CMD ["/run.sh"]
--- run.sh
+++ run.sh
@@ -3,9 +3,21 @@ echo "Hello, world! The time is $(date)."
 
 
 # Test environment variables
-source environmets/config.sh
+source ./environments/config.sh
 
 # Iterate through and print all environment variables
 for var in $(compgen -A variable); do
   echo "$var=${!var}"
 done
+
+
+echo "Installing darkhttpd..."
+# Install darkhttpd
+apk add --no-cache darkhttpd
+echo "darkhttpd installed."
+
+echo "Serving files..."
+# Serve files from the current directory on port 8080
+darkhttpd . --port 8080
+echo "Files served."
+
