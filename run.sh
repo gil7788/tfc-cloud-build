53d5cf1958de16fafbe27a9a2e71e54d1882ffa6
gil7788 <gil7788@gmail.com>
2024-01-25 14:42:02 +0200

    Enhance run.sh
    - Install and serve http files on port 8080 with darkhttpd
    - Copy config.sh to container with docker file

--- run.sh
+++ run.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 echo "Hello, world! The time is $(date)."
 
-
 # Test environment variables
-source environmets/config.sh
+source ./environments/config.sh
 
 # Iterate through and print all environment variables
 for var in $(compgen -A variable); do
   echo "$var=${!var}"
 done
+
+echo "Installing darkhttpd..."
+# Install darkhttpd
+apk add --no-cache darkhttpd
+echo "darkhttpd installed."
+
+echo "Serving files..."
+# Serve files from the current directory on port 8080 in the background
+darkhttpd . --port 8080 &
+
+# Sleep for 10 seconds
+sleep 10
+
+# Terminate the darkhttpd server after 10 seconds
+killall darkhttpd
+
+echo "Files served."
