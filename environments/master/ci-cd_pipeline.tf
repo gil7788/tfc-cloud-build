6330909ccd5cc24b3e16d7bc3e48c269a083e61b
gil7788 <gil7788@gmail.com>
2024-01-26 15:31:47 +0200

    Change CI/CD Pipeline Flow - Use Terraform Cloud Github Action hook only

---
+++ environments/master/ci-cd_pipeline.tf
@@ -0,0 +1,33 @@
+resource "null_resource" "create_user_service" {
+  provisioner "local-exec" {
+    command = "bash ./environments/create_user_services.sh"
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
+resource "null_resource" "generate_substitutions" {
+  provisioner "local-exec" {
+    command = "bash ./environments/generate_substitutions.sh"
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  depends_on = [null_resource.create_user_service]
+}
+
+resource "null_resource" "run_cloudbuild" {
+  provisioner "local-exec" {
+    command = "gcloud builds submit --config tfc_cloudbuild.yaml ."
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  depends_on = [null_resource.generate_substitutions]
+}
